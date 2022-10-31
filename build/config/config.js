a2cbb4a205a6472da918d993a2e3690b7c66c6f5
solomonbarayev <sbarayev2@gmail.com>
2022-10-31 18:46:42 +0200

    add build directory to git ignore

--- build/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-require('dotenv').config({ path: '../../.env' });
-exports.config = {
-    db: process.env.MONGO_DB || 'mongodb://localhost:27017/reportrDB',
-    port: Number(process.env.PORT) || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'secret'
-};
--- build/middleware/errorHandler.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = statusCode === 500 ? 'An error has occurred on the server' : err.message;
-    res.status(statusCode).send({ message });
-    next();
-};
-exports.default = errorHandler;
